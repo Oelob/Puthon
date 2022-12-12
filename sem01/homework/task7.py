f35bbc72295f0d4fd58bee68e88fb881a9a4c6b4
Ivan <ab09@inbox.ru>
2022-12-13 01:36:19 +0500

    решена задача 7

--- sem01/homework/task7.py
+++ sem01/homework/task7.py
@@ -1,6 +1,12 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+x = 0
+y = 0
+z = 0
 
-# for i in range(0,1):
-#     for j in range(0,1):
-#         for t in range(0,1):
-#             if (x and y and z) == 
+for x in range(0,1):
+    for y in range(0,1):
+        for z in range(0,1):
+            if -(x and y and z) == -x or -y or -z:
+                print(True) 
+            else:
+                print(False)
