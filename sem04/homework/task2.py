4538ec5a8070575da389c8bc99a3a5c44909f646
Ivan <ab09@inbox.ru>
2022-12-26 23:38:41 +0500

    task2 completed

--- sem04/homework/task2.py
+++ sem04/homework/task2.py
@@ -4,17 +4,10 @@ a = int(input('Введите число, простые множители ко
 
 list = []
 while a != 1:
-    if a%2 == 0:
-        a = a/2
-        list.append(2)
-    elif a%3 == 0:
-        a = a/3
-        list.append(3)
-    elif a%5 == 0:
-        a = a/5
-        list.append(5)
-    elif a%7 == 0:
-        a = a/7
-        list.append(7)
-    
+    for i in range(2,1000000):
+        if a%i == 0:
+            a = a/i
+            list.append(i)
+            break
+     
 print(list)
